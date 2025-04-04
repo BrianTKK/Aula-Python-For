f7b41ee71fa3507562c2c397956d7a94ab84f7e7
Brian <brianhenriquedp@gmail.com>
2025-04-04 19:25:29 -0400

    Exemplo 1

---
+++ main.py
@@ -0,0 +1,4 @@
+# Cria a variavel numero e com o range, imprime os números 20 vezes. Usando no print (numero + 1) é possivel começar a contagem a partir do numero 1
+
+for numero in range(20):
+    print(numero + 1)
